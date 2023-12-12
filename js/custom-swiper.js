ce28750bf02fc17bcc7a0861f2095f5e7a1838f3
siyazoff <83133133+siyazoff@users.noreply.github.com>
2023-12-12 18:50:17 +0600

    first

---
+++ js/custom-swiper.js
@@ -0,0 +1,45 @@
+const swiperMainMenu = [
+  "Vectrum&nbsp;PRO",
+  "Препараты",
+  "О&nbsp;компании",
+  "Услуги",
+  "Новости",
+  "Контакты",
+];
+
+const swiperMain = new Swiper(".swiper-main", {
+  direction: "vertical",
+  mousewheel: {
+    releaseOnEdges: true,
+  },
+  // effect: "cube",
+  speed: 1000,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return (
+        '<div class="' + className + '">' + swiperMainMenu[index] + "</div>"
+      );
+    },
+  },
+
+  on: {
+    slideChange: function () {
+      var activeIndex = this.activeIndex;
+      let changeBlueItems = `.header__logo, 
+                            .swiper-pagination-bullet, 
+                            .header__sandwich_btn, 
+                            .header__contacts, 
+                            .header__menu_btn, 
+                            .year`;
+
+      if ([1, 3, 4, 5].includes(activeIndex)) {
+        $(changeBlueItems).toggleClass("change_blue");
+      } else {
+        $(changeBlueItems).removeClass("change_blue");
+      }
+    },
+  },
+});
---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const $sandwichMenu = $(".sandwich_menu");
+  const $menuBtn = $(".header__menu_btn");
+  const $menuBtnClose = $(".header__menu_btn_close");
+  const $telNumberInput = $("input[id='tel-number']");
+
+  function toggleActiveElements() {
+    $sandwichMenu.add($menuBtn).add($menuBtnClose).toggleClass("active");
+  }
+
+  function addActiveClass() {
+    $menuBtn.add($sandwichMenu).add($menuBtnClose).addClass("active");
+  }
+
+  function removeActiveClass() {
+    $menuBtnClose.add($sandwichMenu).add($menuBtn).removeClass("active");
+  }
+
+  $menuBtn.add(".header__sandwich_btn").click(toggleActiveElements);
+  $menuBtn.click(addActiveClass);
+  $menuBtnClose.click(removeActiveClass);
+
+  $telNumberInput.mask("+7 (999) 999-99-99");
+});
