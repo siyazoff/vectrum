38733bd3410593fed4901f31c5a180ca5619a005
siyazoff <83133133+siyazoff@users.noreply.github.com>
2023-12-20 18:21:38 +0600

    change destroySwiper logic and add new page

--- js/custom-swiper.js
+++ js/custom-swiper.js
@@ -1,6 +1,7 @@
 let swiperMain = null;
+let swiperVaccine = null;
 
-function initSwiper() {
+function initSwiperPage() {
   const swiperMainMenu = [
     "Vectrum&nbsp;PRO",
     "Препараты",
@@ -44,36 +45,78 @@ function initSwiper() {
         }
       },
     },
+  });
 
-    breakpoints: {
-      320: {
-        freeMode: true,
-        slidesPerView: "auto",
-        autoHeight: true,
+  const swiperVaccineMenu = [
+    "Для&nbsp;птиц",
+    "Иммунокомплексная",
+    "Векторные",
+    "Живые",
+    "Инактивированные",
+    "Контакты",
+  ];
+
+  swiperVaccine = new Swiper(".swiper-main-vaccine", {
+    direction: "vertical",
+    mousewheel: {
+      releaseOnEdges: true,
+    },
+    freeMode: false,
+    speed: 1000,
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return (
+          '<div class="' +
+          className +
+          '">' +
+          swiperVaccineMenu[index] +
+          "</div>"
+        );
       },
-      768: {
-        slidesPerView: "auto",
-        freeMode: true,
-        autoHeight: true,
-        slidesPerView: 1,
+    },
+
+    on: {
+      slideChange: function () {
+        var activeIndex = this.activeIndex;
+        let changeBlueItems = `.header__logo,
+                            .swiper-pagination-bullet,
+                            .header__sandwich_btn,
+                            .header__contacts,
+                            .header__menu_btn,
+                            .year`;
+
+        if ([1].includes(activeIndex)) {
+          $(changeBlueItems).addClass("change_blue");
+        } else if ([0].includes(activeIndex)) {
+          $(changeBlueItems).removeClass("change_blue");
+        } else {
+          $(changeBlueItems).addClass("change_blue");
+        }
       },
-      1440: {},
     },
   });
 }
 
-function destroySwiper() {
-  if (swiper !== null) {
-    swiper.destroy(true, true);
-    swiper = null;
+function destroySwiperPage() {
+  if (swiperMain !== null) {
+    swiperMain.destroy(true, true);
+    swiperMain = null;
+  }
+
+  if (swiperVaccine !== null) {
+    swiperVaccine.destroy(true, true);
+    swiperVaccine = null;
   }
 }
 
 function handleSwiper() {
   if (window.innerWidth <= 768) {
-    destroySwiper();
+    destroySwiperPage();
   } else {
-    initSwiper();
+    initSwiperPage();
   }
 }
 
@@ -82,3 +125,40 @@ handleSwiper();
 window.addEventListener("resize", function () {
   handleSwiper();
 });
+
+const buildSwiperSlider = (sliderElm) => {
+  let sliderIdentifier = sliderElm.dataset.id;
+
+  return new Swiper(`.swiper-immunocomplex-${sliderIdentifier}`, {
+    slidesPerView: 3,
+    direction: "horizontal",
+    grid: {
+      rows: 3,
+      fill: "row",
+    },
+    grabCursor: true,
+    spaceBetween: 0,
+    pagination: {
+      el: `.swiper-immunocomplex-pagination-${sliderIdentifier}`,
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: `.swiper-immunocomplex-button-next-${sliderIdentifier}`,
+      prevEl: `.swiper-immunocomplex-button-prev-${sliderIdentifier}`,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.25,
+        grid: false,
+      },
+      768: {},
+      1440: {},
+    },
+  });
+};
+
+// Get all of the swipers on the page
+const allSliders = document.querySelectorAll(".swiper-immunocomplex");
+
+// Loop over all of the fetched sliders and apply Swiper on each one.
+allSliders.forEach((slider) => buildSwiperSlider(slider));
